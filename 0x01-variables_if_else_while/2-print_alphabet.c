5c14d6e8245d6e71ade594e340e417f92bac50b2
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-16 16:24:32 -0700

    Add File : 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	putchar(letter);
+}
+putchar('\n');
+return (0);
+}
