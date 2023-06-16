3ae7dcbf14cec1eda4f6f68a730a28471ff2c4d5
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-16 16:45:57 -0700

    Add File : 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+	putchar(num+'0');
+}
+putchar('\n');
+return (0);
+}
