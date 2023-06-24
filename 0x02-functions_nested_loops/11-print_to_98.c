5232c93b599aae7f65639724b0f39c380c7cf392
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-24 11:48:26 -0700

    Add File : 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,9 +2,10 @@
 
 void print_to_98(int n)
 {
+	int i;
 	if (n <= 98)
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
@@ -15,7 +16,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
             		printf("%d", i);
 			if (i != 98)
