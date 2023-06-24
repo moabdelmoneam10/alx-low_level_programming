f5479b9e361255074a190bac4c8e8a55c976e3ca
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-24 08:09:34 -0700

    Add File : 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,36 +4,36 @@
 */
 void times_table(void)
 {
-	int i , j ;
+	int i, j;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0 ; j < 10; j++)
 		{
 			int m = i * j;
+
 			if (m < 10)
-			{	
+			{
 				_putchar(m + '0');
 				if (j != 9)
 				{
-					_putchar(',');			
-					_putchar(' ');			
+					_putchar(',');
+					_putchar(' ');
 				if (i * (j + 1) < 10)
-					_putchar(' ');			
+					_putchar(' ');
 				}
 			}
-			else 
+			else
 			{
 				_putchar(m * 0.1 + '0');
 				_putchar(m % 10 + '0');
 				if (j != 9)
-				{	
-					_putchar(',');			
-					_putchar(' ');			
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 		}
-	
 		_putchar('\n');
-	
 	}
 }
