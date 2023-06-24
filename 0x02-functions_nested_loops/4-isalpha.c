cdca373673388719b15b286381cb1ab592ac43b1
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-24 05:18:20 -0700

    Add File : 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check the lowercase character
+ * @c: is the character we check
+ * Return: 1 c is character
+ * Returns 0 otherwise
+*/
+int _isalpha(int c)
+{
+if ((c > 96 && c < 123) || (c < 91) && (c > 64))
+	return (1);
+else
+	return (0);
+}
