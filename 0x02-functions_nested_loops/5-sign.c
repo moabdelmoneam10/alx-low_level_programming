1d8851ed27ce0d6dbd7c9cf6b2ba607e1dd56c4e
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-24 05:25:45 -0700

    Add File : 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - check the lowercase character
+ * @n: is the number we check the sign
+ * Return: 1 c is lowercase
+ * Returns 0 otherwise
+*/
+int print_sign(int n)
+{
+if (n > 0)
+	return (1);
+else if (n == 0)
+	return (0);
+else 
+	return (-1);
+}
