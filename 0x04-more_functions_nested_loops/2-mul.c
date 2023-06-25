f6e07d24de0d109ff64510b4cef966c379b1b61d
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-25 05:03:48 -0700

    Add File : 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply two numbers
+ * @num_1: first num
+ * @num_2: secound num
+ * Return: the sum
+ */
+int mul(int num_1, int num_2)
+{
+return (num_1 * num_2);
+}
