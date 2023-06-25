c824da30d040bfd1427df5fc6dac5bf9cc492e7a
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-25 06:19:57 -0700

    Add File : 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - print every min in the day
+ *
+ * @n: the number of \
+ */
+void print_diagonal(int n)
+{
+int i, j;
+
+if (n > 0)
+{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+else
+	_putchar('\n');
+}
