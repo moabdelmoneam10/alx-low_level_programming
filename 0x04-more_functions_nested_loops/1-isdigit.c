9b086342ffce167f0cd3703da74678aa84713d05
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-25 04:55:30 -0700

    Add File : 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - check the lowercase character
+ * @c: is the character we check
+ * Return: 1 c is character
+ * Returns 0 otherwise
+*/
+int _isdigit(int c)
+{
+if ((c >=0 && c < 9))
+	return (1);
+else
+	return (0);
+}
