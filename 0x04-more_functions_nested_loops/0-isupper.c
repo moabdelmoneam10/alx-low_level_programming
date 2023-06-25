0cdef580130a719ce2a01e64e9d73d1c7f34e8e1
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-25 04:53:00 -0700

    Add File : 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _islower - check the lowercase character
+ * _isupper - check the lowercase character
  * @c: is the character we check
  * Return: 1 c is lowercase
  * Returns 0 otherwise
 */
-int _islower(int c)
+int _isupper(int c)
 {
 if (c > 64 && c < 91)
 	return (1);
