5573598b6f4c64a6060ddf25a9392a078deb8438
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-25 05:06:45 -0700

    Add File : 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int c;
+
+for (c = 0; c <= 9; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+}
