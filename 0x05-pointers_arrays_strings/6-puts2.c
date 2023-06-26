79c5c4d2b9f67a69da78430d5997e12f8d78f24c
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-26 16:42:06 -0700

    Add File : 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - print the string
+ *
+ * @str: the string we print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; ((str[i] != '\0') && (i % 2 == 0)); i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
