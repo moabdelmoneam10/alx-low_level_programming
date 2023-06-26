47ab75809fc4f7ad9355f9151962e3fddfd1bada
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-26 16:14:43 -0700

    Add File : 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - tests function that prints if integer is positive or negative
+ *
+ * @s: the string we measure
+ *
+ */
+void print_rev(char *s)
+{
+	int i, counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{	
+		counter++;
+		_putchar(s[i]);
+	}
+	 _putchar('\n');
+	for (i = counter - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
