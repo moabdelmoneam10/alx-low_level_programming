9ff940a64d5809c767637b08525d7b963d1ea23a
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-06-26 16:06:03 -0700

    Add File : 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - tests function that prints if integer is positive or negative
+ *
+ * @s: the string we measure
+ *
+ * Return: the number of character
+ */
+int _strlen(char *s)
+{
+	int i, counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		counter++;
+	for (i = counter; i != 0; i--)
+		_putchar(str[i]);
+	_putchar('\n');
+}
