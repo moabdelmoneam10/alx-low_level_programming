476a5134a3adf7539816cdb58a5118cb243b5c94
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-03 07:54:36 -0700

    Add File : 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - function that find the first occurrencce of char
+ *
+ * @s: the string we search in
+ * @c: the char we search for
+ * Return: pointer to dest
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 1, flag = 0;
+
+	while (i)
+	{
+		if (s[i] != c)
+		{
+			i++;
+		}
+		else
+		{
+			flag = i;
+			i = 0;
+		}
+	}
+	if (i != 0)
+		return (NULL);
+	else
+		return (s + flag);
+}
