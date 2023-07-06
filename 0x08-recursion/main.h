b54e3ea59fc5c9eebe1e8986b5bd36594b998407
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-05 18:07:16 -0700

    Add File : main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,7 +3,15 @@
 
 #include <stdio.h>
 int _putchar(char);
+
+int _pow_recursion(int x, int y);
+
 void _print_rev_recursion(char *s);
+
 int _strlen_recursion(char *s);
+
 void _puts_recursion(char *s);
+
+int factorial(int n);
+
 #endif /* MAIN_H */
