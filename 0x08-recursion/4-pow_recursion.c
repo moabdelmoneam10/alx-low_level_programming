eecc2d457fd11ab3f162804b5122013e02fe2100
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-05 18:07:20 -0700

    Add File : 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - return power of the num
+ *
+ * @x: the number we rasied
+ * @y: the power
+ * Return: the power of num
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _power_recursion(x, y--));
+}
