f50a3ec806e7ffc3a9d050c63e1b4a0095bdcaee
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-05 17:49:10 -0700

    Add File : 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,10 +3,11 @@
  * _strlen_recursion - return length of the string
  *
  * @s: the string we measure
+ * Return: the length
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return(0);
-	return(1 + _strlen_recursion(s + 1));
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
