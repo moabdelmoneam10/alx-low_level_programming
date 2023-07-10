766a674de6f0129e2027cc6df3b8a7b45d334444
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-10 07:10:19 -0700

    Add File : 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,12 +19,12 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *) malloc(sizeof(int) * weight);
+		grid[i] = (int *) malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 			{
-				free(grid[j])
+				free(grid[j]);
 			}
 			free(grid);
 			return (NULL);
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (j = 0; j < i; j++)
 	{
-		for (j = 0; j < weidth; j++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
