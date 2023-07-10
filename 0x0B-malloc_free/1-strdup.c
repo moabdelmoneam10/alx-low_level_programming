c36be9099c4d473bc75bbc87869ad2f5be8a9ead
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-10 05:22:47 -0700

    Add File : 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  * size - return the size of string
+  *
+  * @p: pointer to first index of string
+  * Return: size
+  */
+int sizes(char *p)
+{
+if (*p == '\0')
+	return (0);
+return (1 + sizes(p + 1));
+
+}
+/**
+ *  _strdup - duplicate strings
+ *
+ * @str: the string we duplicate
+ * Return: pointer to the new string
+ */
+char *_strdup(char *str)
+{
+	int size, i;
+	char *ar;
+
+	size = sizes(str);
+	ar = malloc(sizeof(char) * size);
+	if (ar == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		ar[i] = str[i];
+		i++;
+	}
+
+	return (ar);
+}
+
