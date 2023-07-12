352ca78f096ebae2779db17bdcffb1d958202839
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-12 09:39:34 -0700

    Add File : 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - the main function
+ * @argc: the counter of argument
+ * @argv: hte arguments
+ *
+ * Return: always return 0 for success
+ */
+int main(int argc __attribute__((unused)), char *argv[] )
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
