0a53202150bab652dee978d4f685f7ae92273916
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-12 10:52:59 -0700

    Add File : 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - the main function
+ * @argc: the counter of argument
+ * @argv: hte arguments
+ *
+ * Return: always return 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+	char *arg;
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		arg = argv[i];
+		for (j = 0; arg[j] != '\0'; j++)
+		{
+			if (!isdigit(arg[j]))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
