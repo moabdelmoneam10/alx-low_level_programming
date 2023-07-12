e32c91931f570adb2c733de3c220abcc4ebf9120
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-12 09:30:10 -0700

    Add File : 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - the main function 
+ *
+ *
+ * Return: always return 0 for success 
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
