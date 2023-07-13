43d9f04933dc73cf0bc098b5aed0487fc566ef0e
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-13 16:53:01 -0700

    Add File : 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,6 +20,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
+		if (s1 == NULL)
+			s1 = "";
+		else if (s2 == NULL)
+			s2 = "";
 		i = 0;
 		while (s1[i] != '\0')
 		{
