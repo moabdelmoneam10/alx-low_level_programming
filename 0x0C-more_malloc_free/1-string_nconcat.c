b8ebe002d7a35d3b121be6e68c1340032e6b3e8e
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-13 16:47:29 -0700

    Add File : 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat - concatonate n char
+ *
+ * @s1: the pointer for first string
+ * @s2: the pointer for second string
+ * @n: the number cha from second string
+ * Return: pointer to storage
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ar;
+	int i, j;
+
+	ar = malloc(strlen(s1) + n);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
+		while (s1[i] != '\0')
+		{
+			ar[i] = s1[i];
+			i++;
+		}
+		j = 0;
+		while (j < n)
+		{
+			ar[i] = s1[i];
+			j++;
+		}
+		return (ar);
+	}
+
