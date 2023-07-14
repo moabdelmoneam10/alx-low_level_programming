48df6fa9536c84efef4089f29385969449998951
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-13 17:34:05 -0700

    Add File : 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,28 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * _memset - function that fills memory with a constant byte
+ * @s: memory area pointed to by s
+ * @b: constant value
+ * @n: nulber of byte
+ * Return: pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int u;
+
+		for (u = 0; u < size; u++)
+		{
+			s[u] = b;
+		}
+	}
+	return (s);
+}
+/**
  * _calloc - create array
  *
  * @nmemb: the char we fell with
@@ -17,13 +39,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ar = malloc(nmemb * size);
 	if (ar == NULL)
 		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		ar[i] = 0;
-		i++;
-	}
-
+	_memset(ptr, 0, nmemb * size);
 	return (ar);
 }
 
