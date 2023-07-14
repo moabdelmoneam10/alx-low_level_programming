6bb73ac75a6655ad025f643ef50f330a970934b9
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-13 17:47:24 -0700

    Add File : 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ar = malloc(sizeof(ar) * (max - min));
+	ar = malloc(sizeof(ar) * (max - min) + 1);
 	if (ar == NULL)
 		return (NULL);
 	i = 0;
