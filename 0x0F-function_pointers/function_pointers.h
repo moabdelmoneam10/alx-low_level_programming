2eb438ca5d46d2610da2b57c588682438786e1ae
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-19 09:57:45 -0700

    Add File : function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char );
 #endif /* _FUNCTION_POINTERS_H */
