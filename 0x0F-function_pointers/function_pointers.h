f45c2707c01938044ff0471af94943af4fc23fdb
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-19 09:36:21 -0700

    Add File : function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+void print_name(char *name, void (*f)(char *));
+int _putchar(char );
+#endif /* MAIN_H */
