cefaf8d56d34972a5d6d91f9c9680687b12c840c
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-19 09:44:00 -0700

    Add File : function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char );
-#endif /* FUNCTION_POINTERS_H */
+#endif /* _FUNCTION_POINTERS_H */
