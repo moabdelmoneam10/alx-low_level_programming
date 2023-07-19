3083852268d7bef167c5548ff10460208478b3aa
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-07-19 10:25:30 -0700

    Add File : 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- * print_name - create array
- * @f: function pointer to a function that takes a char* argument
- *
- * @name: the word we work with
+ * array_iterator - function to print array by another function
+ * @array: what we print
+ * @size: the size of the array
+ * @action: a pointer to the function you need to use
  * Return: No thing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
