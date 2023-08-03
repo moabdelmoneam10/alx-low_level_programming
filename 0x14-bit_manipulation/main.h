f260656ed8d7b39d1b2392d582f345feb4735473
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-08-03 10:52:03 -0700

    Add File : main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char );
+
+unsigned int binary_to_uint(const char *b);
+#endif /* MAIN_H */
