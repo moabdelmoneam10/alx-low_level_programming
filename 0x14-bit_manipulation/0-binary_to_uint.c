f0cd00205a7fedf51a511651d71c48bd2b8ff39e
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-08-06 12:51:16 -0700

    Add File : 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, num;
+	unsigned int num;
 
 	num = 0;
 	if (b == NULL)
 		return (0);
-		if (*b == '0' || *b == '1')
-			num = num * 2 + (*b++ - '0');
-		else
-			return (0);
+	while (*b != '\0')
+	{
+	if (*b == '0' || *b == '1')
+	{
+		num = (num * 2) + (*b++ - '0');
 	}
+	else
+		return (0);
+	}
+	
 	return (num);
 }
