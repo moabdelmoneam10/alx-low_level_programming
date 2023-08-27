a9b6383e84604453c76c3e0f1b0b329f9c0222c8
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-08-27 16:51:24 -0700

    Add File : 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
- * reset_to_98 - tests function that prints if integer is positive or negative
- *
- *
- * @n: the resetable number
- */
+  * reset_to_98 - functionn to reset the pointer value
+  * @n: the pointer to number
+  */
 void reset_to_98(int *n)
 {
 	*n = 98;
