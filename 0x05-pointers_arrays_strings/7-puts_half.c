774a493442eec1ce3c070eda3833d236ed80f248
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-08-27 18:42:32 -0700

    Add File : 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 /**
- * puts_half - tests function that prints if integer is positive or negative
+ * puts_half - Entry point.
+ * @str: pointer
  *
- * @str: the string we measure
- *
- */
+*/
+
 void puts_half(char *str)
 {
-	int i, counter = 0;
+int len = strlen(str);
+int n, i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		counter++;
-	if (counter % 2 == 0)
-		counter /= 2;
-	else
-	for (n = (len - 1) / 2; n < len - 1; n++)
-	{
-	_putchar(str[n + 1]);
-	}
-	_putchar('\n');
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+else if (len % 2)
+{
+for (n = (len - 1) / 2; n < len - 1; n++)
+{
+_putchar(str[n + 1]);
+}
+}
+
+_putchar('\n');
 }
