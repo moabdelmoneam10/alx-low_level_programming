1427e8f170a0ea78b6c5c1a7faca0a5bec252486
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-08-27 17:14:38 -0700

    Add File : 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * _strlen - tests function that prints if integer is positive or negative
- *
- * @s: the string we measure
- *
- * Return: the number of character
- */
+  * _strlen - get the length of the string
+  * @s: the string
+  * Return: the length of string
+  */
+
 int _strlen(char *s)
 {
-	int i, counter = 0;
+	int cnt;
 
-	for (i = 0; s[i] != '\0'; i++)
-		counter++;
-	return (counter);
+	for (cnt = 0; *s != '\0'; cnt++, s++);
+	return (cnt);
 }
