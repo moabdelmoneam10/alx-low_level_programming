7cf3a7536ec612163a902240c1bc477967eb8b42
mo.abdelmoneam <m.abdelmoneam10@gmail.com>
2023-08-27 18:15:04 -0700

    Add File : 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,38 @@
 #include "main.h"
-/**
- *  _puts - print the string
- *
- * @str: the string we print
- */
-void _puts(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	_putchar('\n');
+/**
+  * _strlen - get the length of the string
+  * @s1: the string
+  * Return: the length of string
+  */
+
+int _strlen(char *s1)
+{
+	int cnt;
+
+	for (cnt = 0; *s1++ != '\0'; cnt++)
+	{}
+	return (cnt);
+}
+
+/**
+  * rev_string - print the string in rev
+  * @s: the string
+  */
+
+void rev_string(char *s)
+{
+	int len, i;
+	char s2[100];
+
+	i = 0;
+	len = _strlen(s);
+	while (s[i] != '\0')
+	{
+		s2[i] = s[i];
+		i++;
+	}
+	i = 0;
+	while (len--)
+		s[i++] = s2[len];
 }
